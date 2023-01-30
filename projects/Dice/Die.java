eab9c888515ca439f51f66c3c550c0478602581d
Yeeun Shin <122763145+YeeunS@users.noreply.github.com>
2023-01-29 21:10:37 -1000

    Add files via upload

---
+++ projects/Dice/Die.java
@@ -0,0 +1,60 @@
+package edu.ics111.h10;
+
+/**
+ * Represents a single die.
+ * Credit from Professor Cam Moore, textbook javanotes, and TA Yang Qian.
+ * @author Ye Eun Shin
+ */
+public class Die {
+  private int value;
+  private int sides;
+
+  /**
+   * Creates a default 6 sided die.
+   */
+  public Die() {
+    this(6);
+    roll();
+  }
+
+  /**
+   * Creates a die with the given number of sides.
+   * 
+   * @param sides The number of sides for the die.
+   */
+  public Die(int sides) {
+    this.sides = sides;
+    roll();
+  }
+
+  /**
+   * Rolls the die.
+   * 
+   * @return the new value of the die.
+   */
+  public int roll() {
+    value = (int) (Math.random() * sides + 1);
+    return value;
+  }
+
+  /**
+   * Returns the value.
+   * 
+   * @return the value.
+   */
+  public int getValue() {
+    return value;
+  }
+
+  /**
+   * Returns the sides.
+   * 
+   * @return the sides.
+   */
+  public int getSides() {
+    return sides;
+  }
+}
+
+
+
